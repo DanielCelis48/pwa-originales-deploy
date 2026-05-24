cd585cbdc37d0574f65e63d7af3cc4aac9f0ff2b
DanielCelis48 <daniel.celisg@uniagustiniana.edu.co>
2026-05-24 19:50:55 +0000

    V1-Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,33 @@
 import "./globals.css"
+
 import Navbar
 from "./components/Navbar"
+
+import type {
+Metadata
+} from "next"
+
+export const metadata:Metadata={
+
+title:"Zona XP",
+
+description:
+"Explora videojuegos, guarda favoritos y crea tu biblioteca gamer.",
+
+manifest:"/manifest.json",
+
+icons:{
+
+icon:"/icon-192.png",
+
+apple:
+"/apple-touch-icon.png"
+
+},
+
+themeColor:"#4f46e5"
+
+}
 
 export default function RootLayout(
 {
@@ -13,12 +40,16 @@ React.ReactNode
 
 return(
 
-<html>
+<html
+lang="es"
+>
 
 <body
 className="
 bg-slate-950
-text-white"
+text-white
+min-h-screen
+"
 >
 
 <Navbar/>
